f23a66a672cef82745b32045761bfabe32893ce8
geleazar1000111 <geraldine@MacBook-Pro.local>
2020-09-10 20:34:23 -0700

    test pr for ballotnav

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,10 @@ def hello():
 def hello(): 
 	return "Operational"
 
+@app.route('/test')
+def test():
+	return "This is for my test PR"
+
 
 if __name__ == "__main__": 
 	app.run(host ='0.0.0.0', port = 80, debug = True) 
