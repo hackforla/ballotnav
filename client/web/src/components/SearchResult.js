6fb35fe75047499064b62a2aecd0d02798eb89cb
jacobzwang <zwang.jacob@gmail.com>
2020-09-12 21:32:04 -0700

    restyled sidebar /  cleaned code

--- client/web/src/components/SearchResult.js
+++ client/web/src/components/SearchResult.js
@@ -4,22 +4,14 @@ import "./SearchResult.css";
 export default function SearchResult() {
   return (
     <div className="searchResult">
-      <h1>601 Pico Blvd Santa Monica 90405</h1>
-      <hr></hr>
-      <div className="row">
-        <div>
-          <h2>
-            <a href="https://www.santamonica.org">www.santamonica.org</a>
-          </h2>
-          <h2>
-            <a href="https://www.santamonica.org">source</a>
-          </h2>
-        </div>
-        <div>
-          <h2>555-5771-243</h2>
-          <h2>6:00 AM - 8:00 PM</h2>
-        </div>
+      <h1>William Faria Elementary Shool</h1>
+      <div>
+        <p>10155 Barbara Ln, Cupertino, CA 95014</p>
       </div>
+      <p>
+        <span className="inline">0.4 mile</span>{" "}
+        <span className="inline">20 min</span>
+      </p>
     </div>
   );
 }
