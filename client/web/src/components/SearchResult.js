f0e6274242527c2712ef6b4de60cb4bf17d6edad
JacobZwang <59858341+JacobZwang@users.noreply.github.com>
2020-09-13 13:50:27 -0700

    cleaned and commented code (#51)
    * cleaned and commented code
    
    * Update client/web/src/pages/Home.js
    
    Co-authored-by: Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>

--- client/web/src/components/SearchResult.js
+++ client/web/src/components/SearchResult.js
@@ -4,12 +4,12 @@ import "./SearchResult.css";
 export default function SearchResult() {
   return (
     <div className="searchResult">
+      {/* dynamic data should be passed in as these value */}
       <h1>William Faria Elementary Shool</h1>
-      <div>
-        <p>10155 Barbara Ln, Cupertino, CA 95014</p>
-      </div>
+      {/* each <p> tag is 1 row of data */}
+      <p>10155 Barbara Ln, Cupertino, CA 95014</p>
       <p>
-        <span className="inline">0.4 mile</span>{" "}
+        <span className="inline">0.4 mile</span>
         <span className="inline">20 min</span>
       </p>
     </div>
