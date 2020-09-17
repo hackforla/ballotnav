4b4383220b54ad53296b60361525ff67dd6e50eb
jafow <jared.a.fowler@gmail.com>
2020-09-17 10:24:57 -0700

    Fix db bootstrap for migrations
    remove examle sql

--- backend/app.js
+++ backend/app.js
@@ -9,15 +9,14 @@ const fetch = require("node-fetch");
 const app = express();
 const router = express.Router();
 const port = 8080;
-const db = require("./db/database")
-const Dropoffs = require("./models/Dropoffs");
-
-db.authenticate().then(() => console.log('Database connected')).catch(err => console.log("Error "+ err))
+const Dropoffs = require("./models/Dropoffs.js");
 
 router.use(function (req, res) {
   console.log("/" + req.method);
  res.sendStatus(200);
 });
+
+app.use("/", router);
 
 
 router.get(function (req, res) {
@@ -46,30 +45,6 @@ app.get("/dropoff/:state.:state_county", function(req, res) {
       res.send({"Error": "Error occurred"})
     });
 });
-
-app.get('/', async function(req, res) {
-  try {
-    res.send(
-      (async () =>{
-        const locationdata = await csv().fromFile("Kansas County Officials - data.csv")
-        const access_token = "pk.eyJ1IjoiYmFsbG90bmF2IiwiYSI6ImNrZjAycnpldzBzdzkzMW51eGdwOWxtaWIifQ.KlkcMSLbgrj8qkX2_RSaog";
-    
-        for(i = 1; i < locationdata.length; i++){
-            
-            fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${locationdata[i].field5}.json?access_token=${access_token}`)
-      .then(response => response.json())
-      .then(json => console.log(json.features[1].center))
-           
-        }
-        
-    })()
-    );
-  } catch (err) {
-    res.json({err})
-  }
-}),
-
-app.use("/", router);
 
 app.listen(port, function () {
   console.log("App listening on port 8080!");
