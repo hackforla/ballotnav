e9f0049b6d43ad91c78ae04b172ebec9ab75a651
Jake Mensch <jake.mensch@gmail.com>
2020-09-18 17:38:15 -0700

    /seed, /dropoff and /dropoffs  working (#83)

--- backend/app.js
+++ backend/app.js
@@ -7,22 +7,30 @@ const csv = require('csvtojson');
 const fetch = require("node-fetch");
 
 const app = express();
+app.use(express.json());
 const router = express.Router();
 const port = 8080;
 const Dropoffs = require("./models/Dropoffs.js");
 const db = require('./models/index');
+const loadDropoffs = require('./seeders/loadDropoffs');
 
 app.get("/status", (req, res) => {
   res.send(`ok at ${new Date()}`)
 })
 
+app.get("/seed", (req, res, next) => {
+  db.sequelize.sync({ force: true }) // reset db
+    .then(loadDropoffs)
+    .then(() => res.json({ message: "database seeded" }))
+    .catch(next)
+})
 
 app.get("/dropoff/:state.:state_county", function(req, res) {
   let state = req.params["state"];
   let state_county = req.params["state_county"];
   db.Dropoffs.findAll(
     {where: {state_short_code: state,
-    county: state_county}}).then( function(dropoffs) 
+    county: state_county}}).then( function(dropoffs)
     {
         if (!dropoffs) {
           res.send([])
@@ -32,6 +40,29 @@ app.get("/dropoff/:state.:state_county", function(req, res) {
       res.send({"Error": "Error occurred"})
     });
 });
+
+app.post("/dropoff", (req, res, next) => {
+  db.Dropoffs.create(req.body)
+    .then(data => res.json(data))
+    .catch(next)
+})
+
+app.post("/dropoffs", (req, res, next) => {
+  db.Dropoffs.bulkCreate(req.body, { validate: true })
+    .then(data => res.json(data))
+    .catch(next)
+})
+
+// handle errors
+app.use((err, req, res, next) => {
+  if (
+    err instanceof db.Sequelize.ValidationError ||
+    err instanceof db.Sequelize.AggregateError
+  )
+    res.status(400).send(err)
+  else
+    res.status(500).send(err.message)
+})
 
 app.listen(port, function () {
   console.log("App listening on port 8080!");
