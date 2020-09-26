2edc8289dfdf42cb6dd47f2d8345a502116fbfbe
tan-nate <nktan93@gmail.com>
2020-09-26 11:41:47 -0700

    working on Home page

--- client/web/src/components/main/Home.jsx
+++ client/web/src/components/main/Home.jsx
@@ -2,67 +2,16 @@ import React from "react";
 
 import Logo from "../../assets/ballotnav-logo.png";
 import cloudImage from '../../assets/cloud-image.svg'
-import Hero from "../../assets/ballotnavHero.svg";
 import codeForAmericaLogo from "../../assets/codeForAmericaLogo.png";
 import hackForLaLogo from "../../assets/hackForLaLogo.png";
 
 export default function Home() {
   return (
-    <div className="landingPage">
-      <div className="landingFlex">
-        <div className="landingContentWrapper">
-          <div className="contentCenter">
-            <img
-              src={Logo}
-              alt="BallotNav Logo"
-              className="ballotnavLogoLarge"
-            ></img>
-            <h3>
-              Find safe, secure, in-person locations to
-              <br /> drop off your mail-in or absentee ballot
-            </h3>
-            <div className="landingFlex">
-              <input
-                className="landingInput"
-                placeholder="Enter an address or zip code"
-              ></input>
-              <a href="/">
-                <button className="landingSearchButton">Search</button>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="cloud-image-wrapper">
-          <img
-            src={cloudImage}
-            alt="Cloud Image"
-            className="cloud-image"
-          >
-          </img>
-        </div>
-        <div className="landingHeroWrapper">
-          <img
-            src={Hero}
-            alt="BallotNav Hero"
-            className="ballotnavHero"
-          ></img>
-        </div>
-      </div>
-      <div className="landingFooter">
-        <a href="https://www.hackforla.org/" target="_blank" rel="noopener noreferrer">
-          <img
-            src={hackForLaLogo}
-            alt="BallotNav Logo"
-            className="landingFooterLogo"
-          ></img>
-        </a>
-        <a href="https://www.codeforamerica.org/">
-          <img
-            src={codeForAmericaLogo}
-            alt="BallotNav Logo"
-            className="landingFooterLogo"
-          ></img>
-        </a>
+    <div className="hero-wrapper" role="banner">
+      <div className="cloud-background" />
+      <div className="hero-text-wrapper">
+        <h2>Find your drop off locations</h2>
+        <h3>Find safe, secure, in-person locations to drop off your mail-in or absentee ballot</h3>
       </div>
     </div>
   );
