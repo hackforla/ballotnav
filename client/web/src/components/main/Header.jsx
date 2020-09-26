124960465450630379274eb19e11466fcd7c233b
tan-nate <nktan93@gmail.com>
2020-09-25 18:39:47 -0700

    created Header

--- client/web/src/components/main/Header.jsx
+++ client/web/src/components/main/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import logo from "../../assets/ballotnav-logo.png";
 
 const Header = () => {
   return (
-    <>
-    </>
+    <nav
+      className="navbar"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src={logo} width="112" height="28" />
+        </a>
+
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+    </nav>
   );
 }
 
