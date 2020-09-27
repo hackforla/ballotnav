a422a228588228755f5d474ac8b41abdc9e71d9b
tan-nate <nktan93@gmail.com>
2020-09-26 19:47:20 -0700

    working on geocoder

--- client/web/src/App.jsx
+++ client/web/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import Header from './components/main/Header';
 import Routes from './Routes';
+import Footer from './components/main/Footer';
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes />
+      <Footer />
     </Router>
   );
 }
