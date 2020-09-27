2d6a36c03c0e00cdf14b22b4acede68d978dcb54
Jake Mensch <jake.mensch@gmail.com>
2020-09-27 11:22:57 -0700

    tabs in edit jurisdiction

--- client/web-admin-new/src/components/Dashboard/EditJurisdiction.js
+++ client/web-admin-new/src/components/Dashboard/EditJurisdiction.js
@@ -3,10 +3,32 @@ import { Link, useParams } from 'react-router-dom'
 import api from 'services/api'
 import model from 'models/jurisdiction'
 import AutoForm from './AutoForm'
+import { Tabs, Tab, Box } from '@material-ui/core'
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          { children }
+        </Box>
+      )}
+    </div>
+  );
+}
 
 function EditJurisdiction() {
   const { jid } = useParams()
   const [jurisdiction, setJurisdiction] = useState(null)
+  const [tabNum, setTabNum] = useState(0)
 
   useEffect(() => {
     api.jurisdictions.getById(jid).then(setJurisdiction)
@@ -15,16 +37,22 @@ function EditJurisdiction() {
   if (!jurisdiction) return null
   return (
     <>
-      <AutoForm
-        model={model}
-        initialValues={null}
-        onSubmit={(values, funcs) => {
-          console.log(values)
-          funcs.setSubmitting(false)
-        }}
-        style={{ maxWidth: 400 }}
-      />
-      <div>
+      <Tabs value={tabNum} onChange={(event, newValue) => setTabNum(newValue)}>
+        <Tab label='Jurisdiction' />
+        <Tab label='Locations' />
+      </Tabs>
+      <TabPanel value={tabNum} index={0}>
+        <AutoForm
+          model={model}
+          initialValues={null}
+          onSubmit={(values, funcs) => {
+            console.log(values)
+            funcs.setSubmitting(false)
+          }}
+          style={{ maxWidth: 400 }}
+        />
+      </TabPanel>
+      <TabPanel value={tabNum} index={1}>
         {jurisdiction.locations.map((loc) => (
           <div key={loc.id}>
             <Link to={`/jurisdictions/${jid}/locations/${loc.id}`}>
@@ -32,7 +60,7 @@ function EditJurisdiction() {
             </Link>
           </div>
         ))}
-      </div>
+      </TabPanel>
     </>
   )
 }
