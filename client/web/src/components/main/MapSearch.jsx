3e793d77df09f7370b4042acab12a1f7dce3170b
tan-nate <nktan93@gmail.com>
2020-09-26 19:10:34 -0700

    working on MapSearch

--- client/web/src/components/main/MapSearch.jsx
+++ client/web/src/components/main/MapSearch.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import searchIcon from '../../assets/search-icon.svg';
+
 class MapSearch extends React.Component {
   render () {
     return (
-      <input id="map-search" class="input" type="text" placeholder="Text input"></input>
+      <div class="field" id="map-search-wrapper">
+        <div class="control" id="search-input">
+          <input id="map-search" class="input" type="text" placeholder="Enter an address or ZIP" />
+        </div>
+        <div class="control">
+          <button class="button is-link">
+            <img src={searchIcon} />
+          </button>
+        </div>
+      </div>
     );
   }
 };
