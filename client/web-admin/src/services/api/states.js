268d7495d5a1f84a7b1d7579500fc288ed8f55e7
Jake Mensch <jake.mensch@gmail.com>
2020-09-27 15:35:47 -0700

    replaced old web-admin with new one

--- client/web-admin/src/services/api/states.js
+++ client/web-admin/src/services/api/states.js
@@ -1,8 +1,51 @@
-import axios from 'axios'
+// import axios from 'axios'
+//
+// const BASE_URL = `${process.env.REACT_APP_API_URL}/states`
 
-const BASE_URL = `${process.env.REACT_APP_API_URL}/states`
+const STATES = [
+  {
+    id: 1,
+    name: 'Wisconsin',
+  },
+  {
+    id: 2,
+    name: 'Pennsylvania',
+  },
+  {
+    id: 3,
+    name: 'Florida',
+  },
+  {
+    id: 4,
+    name: 'Michigan',
+  },
+]
+
+const JURISDICTIONS = [
+  {
+    id: 1,
+    name: 'Geneva County',
+  },
+  {
+    id: 2,
+    name: 'Lee County',
+  },
+  {
+    id: 3,
+    name: 'Monroe County',
+  },
+]
 
 export const list = async () => {
-  const { data } = await axios.get(BASE_URL)
-  return data
+  // const { data } = await axios.get(BASE_URL)
+  // return data
+  return STATES
+}
+
+export const getById = async (id) => {
+  return {
+    id,
+    name: STATES.find((s) => s.id === +id)?.name,
+    jurisdictions: JURISDICTIONS,
+  }
 }
