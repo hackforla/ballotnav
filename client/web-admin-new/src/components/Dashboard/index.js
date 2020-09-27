64ecde7f3007a66452895418671da5f1c2b04534
Jake Mensch <jake.mensch@gmail.com>
2020-09-27 10:10:38 -0700

    new layout

--- client/web-admin-new/src/components/Dashboard/index.js
+++ client/web-admin-new/src/components/Dashboard/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { useAuth } from 'components/use-auth'
+import Layout from './Layout'
 
-import Header from './Header'
 import UserJurisdictions from './UserJurisdictions'
 import EditJurisdiction from './EditJurisdiction'
 import EditLocation from './EditLocation'
@@ -16,22 +16,19 @@ function Dashboard() {
   const { user } = useAuth()
   const isAdmin = user.role === 'admin'
   return (
-    <>
-      <Header />
-      <div style={{ padding: 20 }}>
-        <Switch>
-          <Route exact path='/jurisdictions' component={UserJurisdictions} />
-          { isAdmin && <Route exact path="/jurisdictions/search" component={SearchJurisdictions} /> }
-          <Route exact path="/jurisdictions/:jid" component={EditJurisdiction} />
-          <Route exact path="/jurisdictions/:jid/locations/:lid" component={EditLocation} />
-          <Route exact path="/jurisdictions/:jid/locations/new" component={AddLocation} />
-          { isAdmin && <Route exact path="/states" component={SearchStates} /> }
-          { isAdmin && <Route exact path="/states/:id" component={EditState} /> }
-          { isAdmin && <Route exact path="/review" component={ReviewWIP} /> }
-          <Redirect to='/jurisdictions' />
-        </Switch>
-      </div>
-    </>
+    <Layout>
+      <Switch>
+        <Route exact path='/jurisdictions' component={UserJurisdictions} />
+        { isAdmin && <Route exact path="/jurisdictions/search" component={SearchJurisdictions} /> }
+        <Route exact path="/jurisdictions/:jid" component={EditJurisdiction} />
+        <Route exact path="/jurisdictions/:jid/locations/:lid" component={EditLocation} />
+        <Route exact path="/jurisdictions/:jid/locations/new" component={AddLocation} />
+        { isAdmin && <Route exact path="/states" component={SearchStates} /> }
+        { isAdmin && <Route exact path="/states/:id" component={EditState} /> }
+        { isAdmin && <Route exact path="/review" component={ReviewWIP} /> }
+        <Redirect to='/jurisdictions' />
+      </Switch>
+    </Layout>
   )
 }
 
