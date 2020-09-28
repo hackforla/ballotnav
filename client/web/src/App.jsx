13a378409dd85fea6ebc9124089ad5ad8af45789
tan-nate <nktan93@gmail.com>
2020-09-27 22:45:29 -0700

    footer doesn't display in map

--- client/web/src/App.jsx
+++ client/web/src/App.jsx
@@ -3,14 +3,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import Header from './components/main/Header';
 import Routes from './Routes';
-import Footer from './components/main/Footer';
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes />
-      <Footer />
     </Router>
   );
 }
--- client/web/src/components/main/Home.jsx
+++ client/web/src/components/main/Home.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+
 import HomeMapSearch from './HomeMapSearch';
+import Footer from './Footer';
 
 const Home = ({
   map,
 }) => {
   return (
-    <div className="hero-wrapper" role="banner">
-      <div className="cloud-background" />
-      <div className="hero-text-wrapper">
-        <h2>Find your <br /> drop off locations</h2>
-        <h3>Find safe, secure, in-person locations to drop off your mail-in or absentee ballot</h3>
+    <>
+      <div className="hero-wrapper" role="banner">
+        <div className="cloud-background" />
+        <div className="hero-text-wrapper">
+          <h2>Find your <br /> drop off locations</h2>
+          <h3>Find safe, secure, in-person locations to drop off your mail-in or absentee ballot</h3>
+        </div>
+        <HomeMapSearch map={map} />
       </div>
-      <HomeMapSearch map={map} />
-    </div>
+      <Footer />
+    </>
   );
 }
 
