160ce0e0e27d6a7357f32579aea46cbf1740a245
jafow <jared.a.fowler@gmail.com>
2020-09-27 22:41:44 -0700

    add routes for  wip jurisdictions

--- backend/routes/jurisdictions.js
+++ backend/routes/jurisdictions.js
@@ -6,4 +6,8 @@ router.get('/', control.list)
 router.post('/', auth(['volunteer', 'admin']), control.create)
 router.delete('/', auth(['admin']), control.delete)
 
+router.get('/wip', control.listWip)
+router.post('/wip', auth(['volunteer', 'admin']), control.createWip)
+router.put('/wip', auth(['volunteer', 'admin']), control.updateWip)
+
 module.exports = router
