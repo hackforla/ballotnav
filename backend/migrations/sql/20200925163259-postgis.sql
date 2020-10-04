a979f3fe1fa38f7d609a7760c5001089bf0243a4
aNullValue <drew@anullvalue.net>
2020-10-04 17:29:28 -0400

    Adds convenience PostGIS enablement commands

---
+++ backend/migrations/sql/20200925163259-postgis.sql
@@ -0,0 +1,11 @@
+-- this file provided for convenience only, and would need to be run in a pg instance that is not prebuilt w/ postgis extensions
+CREATE EXTENSION postgis;
+
+CREATE EXTENSION postgis_raster;
+
+CREATE EXTENSION postgis_topology;
+
+CREATE EXTENSION fuzzystrmatch;
+
+CREATE EXTENSION postgis_tiger_geocoder;
+
