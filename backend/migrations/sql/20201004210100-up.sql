096758af49f2dad0449dbf7067917b4da0f126b7
aNullValue <drew@anullvalue.net>
2020-10-04 21:29:05 -0400

    Convert procedures to functions

--- backend/migrations/sql/20201004210100-up.sql
+++ backend/migrations/sql/20201004210100-up.sql
@@ -539,3 +539,118 @@ ALTER TABLE public.wip_jurisdiction_importantdate
 	DROP CONSTRAINT wip_jurisdiction_importantdate_wip_jurisdiction_id_key ,
 	ADD CONSTRAINT wip_jurisdiction_importantdate_uq1 UNIQUE (wip_jurisdiction_id , importantdatetype_id);
 
+DROP PROCEDURE public.fixup_enforced_timezones ();
+
+CREATE OR REPLACE FUNCTION public.fixup_enforced_timezones ()
+	RETURNS boolean
+	LANGUAGE 'plpgsql'
+	AS $BODY$
+BEGIN
+	UPDATE
+		public.jurisdiction
+	SET
+		timezone_default = t0.timezone_default
+		, timezone_enforced = t0.timezone_enforced
+		, is_validated_timezone = TRUE
+	FROM (
+		SELECT
+			j.id
+			, s.timezone_enforced
+			, s.timezone_default
+		FROM
+			public.jurisdiction j
+			INNER JOIN public.state s ON j.state_id = s.id
+		WHERE
+			s.timezone_default IS NOT NULL
+			AND s.timezone_enforced IS TRUE
+			AND (j.timezone_enforced IS NULL
+				OR j.timezone_enforced IS FALSE
+				OR j.timezone_default IS NULL
+				OR j.timezone_default <> s.timezone_default)) t0
+WHERE
+	jurisdiction.id = t0.id;
+	UPDATE
+		public.location
+	SET
+		timezone = t0.timezone_default
+		, is_validated_timezone = TRUE
+	FROM (
+		SELECT
+			l.id
+			, j.timezone_default
+		FROM
+			public.location l
+			INNER JOIN public.jurisdiction j ON l.jurisdiction_id = j.id
+		WHERE
+			j.timezone_default IS NOT NULL
+			AND j.timezone_enforced IS TRUE
+			AND (l.timezone IS NULL
+				OR l.timezone <> j.timezone_default)) t0
+WHERE
+	location.id = t0.id;
+	RETURN TRUE;
+END;
+$BODY$;
+
+DROP PROCEDURE public.generate_calendar (date , integer);
+
+CREATE OR REPLACE FUNCTION public.generate_calendar (from_date date , days integer)
+	RETURNS boolean
+	LANGUAGE 'plpgsql'
+	AS $BODY$
+BEGIN
+	INSERT INTO calendar
+	SELECT
+		TO_CHAR(datum , 'yyyymmdd')::int AS date_dim_id
+		, datum AS date_actual
+		, TRIM(TO_CHAR(datum , 'fmDDth')) AS day_suffix
+		, TRIM(TO_CHAR(datum , 'Day')) AS day_name
+		, EXTRACT(ISODOW FROM datum) AS day_of_week
+		, EXTRACT(DAY FROM datum) AS day_of_month
+		, datum - DATE_TRUNC('quarter' , datum)::date + 1 AS day_of_quarter
+		, EXTRACT(DOY FROM datum) AS day_of_year
+		, TO_CHAR(datum , 'W')::int AS week_of_month
+		, EXTRACT(WEEK FROM datum) AS week_of_year
+		, trim(EXTRACT(ISOYEAR FROM datum) || TO_CHAR(datum , '"-W"IW-') || EXTRACT(ISODOW FROM datum)) AS week_of_year_iso
+		, EXTRACT(MONTH FROM datum) AS month_actual
+		, trim(TO_CHAR(datum , 'Month')) AS month_name
+		, trim(TO_CHAR(datum , 'Mon')) AS month_name_abbreviated
+		, EXTRACT(QUARTER FROM datum) AS quarter_actual
+		, CASE WHEN EXTRACT(QUARTER FROM datum) = 1 THEN
+			'First'
+		WHEN EXTRACT(QUARTER FROM datum) = 2 THEN
+			'Second'
+		WHEN EXTRACT(QUARTER FROM datum) = 3 THEN
+			'Third'
+		WHEN EXTRACT(QUARTER FROM datum) = 4 THEN
+			'Fourth'
+		END AS quarter_name
+		, EXTRACT(ISOYEAR FROM datum) AS year_actual
+		, datum + (1 - EXTRACT(ISODOW FROM datum))::int AS first_day_of_week
+		, datum + (7 - EXTRACT(ISODOW FROM datum))::int AS last_day_of_week
+		, datum + (1 - EXTRACT(DAY FROM datum))::int AS first_day_of_month
+		, (DATE_TRUNC('MONTH' , datum) + INTERVAL '1 MONTH - 1 day')::date AS last_day_of_month
+		, DATE_TRUNC('quarter' , datum)::date AS first_day_of_quarter
+		, (DATE_TRUNC('quarter' , datum) + INTERVAL '3 MONTH - 1 day')::date AS last_day_of_quarter
+		, TO_DATE(EXTRACT(YEAR FROM datum) || '-01-01' , 'YYYY-MM-DD') AS first_day_of_year
+		, TO_DATE(EXTRACT(YEAR FROM datum) || '-12-31' , 'YYYY-MM-DD') AS last_day_of_year
+		, TO_CHAR(datum , 'mmyyyy') AS mmyyyy
+		, TO_CHAR(datum , 'mmddyyyy') AS mmddyyyy
+		, CASE WHEN EXTRACT(ISODOW FROM datum) IN (6 , 7) THEN
+			TRUE
+		ELSE
+			FALSE
+		END AS weekend_indr
+	FROM (
+		SELECT
+			from_date + SEQUENCE.DAY AS datum
+		FROM
+			GENERATE_SERIES(0 , days) AS SEQUENCE (DAY)
+		GROUP BY
+			SEQUENCE.DAY) DQ
+ORDER BY
+	1;
+	RETURN TRUE;
+END;
+$BODY$;
+
