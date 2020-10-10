80f9b5abc7b105a63a9d674913b8de078dec14e6
tan-nate <nktan93@gmail.com>
2020-10-10 15:30:01 -0700

    stubbed out ResultList component

---
+++ client/web/src/components/map/ResultList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Drawer } from 'rsuite';
+
+const ResultList = ({
+  data,
+}) => {
+  return(
+    <Drawer />
+  );
+};
+
+const mapStateToProps = state => ({
+  data: state.data,
+});
+
+export default connect(mapStateToProps)(ResultList);
+
+ResultHeader.propTypes = {
+  data: PropTypes.object,
+}
