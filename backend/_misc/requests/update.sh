516362acf49c09e1969726790417e132858e60dd
jafow <jared.a.fowler@gmail.com>
2020-10-13 12:39:03 -0700

    update test request sample data
    example request payload for update wipjurisdiction

--- backend/_misc/requests/update.sh
+++ backend/_misc/requests/update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # update a jurisdiction
-URL=localhost:8080/jurisdictions/wip/11
+URL=localhost:8080/admin/wip/jurisdictions/11
 
 curl -X PUT -d @update-jurisdiction.json -s -H @headers.txt $URL
 
