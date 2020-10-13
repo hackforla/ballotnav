9d8ad5cd59e4f6fecb8fa5666d3841f6b873cc4e
alexchoiweb <alexchoiweb@gmail.com>
2020-10-12 23:12:46 -0700

    initial css of drawer

--- client/web/src/components/map/ResultHeader.jsx
+++ client/web/src/components/map/ResultHeader.jsx
@@ -22,6 +22,21 @@ const ResultHeader = ({
         <>
           <p>{city}, {state}</p>
           <a>Important election information</a>
+          <div id="election-info">
+            <p>California</p>
+            <span>Last updated: September 14, 2020</span>
+            <p>
+              We have found the most credible and up to date state and EAJ changes to be on <a>vote.org</a>.
+            </p>
+            <a>California Secretary of State</a>
+            <p>Subscribe</p>
+            <p>Last-minute changes to details about how to vote should be expected. Sign up for SMS notifications for updates to your Secretary of State website. Mobile messaging rates may apply.</p>
+            <p>Write your phone number to receive State's updates</p>
+            <form>
+              <input is-info type="tel" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder='(158)-234-6678' />
+              <button is-primary>Subscribe</button>
+            </form>
+          </div>
         </>
       }
     </div>
