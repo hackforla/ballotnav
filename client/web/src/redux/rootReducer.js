f79990d061cb0bf7a4749848e66c5c1227d87adf
tan-nate <nktan93@gmail.com>
2020-10-14 19:56:29 -0700

    created addSelectedLocation redux

--- client/web/src/redux/rootReducer.js
+++ client/web/src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import searches from './reducers/search';
 import data from './reducers/data';
+import ui from './reducers/ui';
 
 export default combineReducers({
   searches,
   data,
+  ui,
 });
