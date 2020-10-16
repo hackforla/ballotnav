4f8e0acc9948df303b499e6c56c9b99ed37ddf51
aNullValue <drew@anullvalue.net>
2020-10-16 11:09:16 -0400

    user_jurisdiction_with_currwip now includes status

---
+++ backend/migrations/sql/20201016105900-up.sql
@@ -0,0 +1,34 @@
+DROP VIEW public.user_jurisdiction_with_currwip;
+
+CREATE OR REPLACE VIEW public.user_jurisdiction_with_currwip AS
+SELECT
+	user_id
+	, jurisdiction_id
+	, state_name
+	, jurisdiction_name
+	, wip_jurisdiction_id
+	, wip_jurisdiction_is_released
+FROM (
+	SELECT
+		uj.user_id
+		, uj.jurisdiction_id
+		, s.name AS state_name
+		, j.name AS jurisdiction_name
+		, wj.id AS wip_jurisdiction_id
+		, wj.is_released AS wip_jurisdiction_is_released
+		, RANK() OVER (PARTITION BY uj.jurisdiction_id ORDER BY wj.id DESC) rank
+	FROM user_jurisdiction uj
+	JOIN "user" u ON uj.user_id = u.id
+	JOIN jurisdiction j ON uj.jurisdiction_id = j.id
+	JOIN state s ON j.state_id = s.id
+	LEFT JOIN wip_jurisdiction wj ON j.id = wj.jurisdiction_id
+		AND u.id = wj.editor_user_id
+		AND (wj.edit_basis_wip_jurisdiction_id IS NULL
+		AND j.wip_jurisdiction_id IS NULL
+		OR wj.edit_basis_wip_jurisdiction_id IS NOT NULL
+		AND j.wip_jurisdiction_id IS NOT NULL
+		AND j.wip_jurisdiction_id = wj.edit_basis_wip_jurisdiction_id)
+WHERE uj.status = 'editor'::enum_user_jurisdiction_status) t0
+WHERE
+	t0.rank = 1;
+
