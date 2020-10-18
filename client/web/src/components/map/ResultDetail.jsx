44cb213ede3da227d93002c29ec3179e8653da86
tan-nate <nktan93@gmail.com>
2020-10-17 20:24:44 -0700

    working on ResultDetail

--- client/web/src/components/map/ResultDetail.jsx
+++ client/web/src/components/map/ResultDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import pinIcon from '../../assets/pin-icon.svg';
 import backArrow from '../../assets/back-arrow-icon.svg';
 import { Drawer } from 'rsuite';
 
@@ -24,7 +25,12 @@ const ResultDetail = ({
       </Drawer.Header>
       <Drawer.Body>
         <h1>{location.name}</h1>
-        <p>Details</p>
+        <div className="result-card-content-wrapper">
+          <img className="address-icon" src={pinIcon} alt="Address icon" />
+          <h4>{location.address1.substring(0, location.address1.length - 1)}, {location.address2}</h4>
+          <br />
+          <h4 className="second-line">{location.address3}</h4>
+        </div>
       </Drawer.Body>
     </Drawer>
   );
