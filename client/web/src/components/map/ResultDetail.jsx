34337ec66388187e6b32ee4e4897ffabaf41d9b1
tan-nate <nktan93@gmail.com>
2020-10-18 23:21:08 -0700

    working on ResultDetail

--- client/web/src/components/map/ResultDetail.jsx
+++ client/web/src/components/map/ResultDetail.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronDown, faClock } from '@fortawesome/free-solid-svg-icons'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+import { Drawer, Dropdown } from 'rsuite';
+import Moment from 'react-moment';
 
 import pinIcon from '../../assets/pin-icon.svg';
 import backArrow from '../../assets/back-arrow-icon.svg';
-import { Drawer, Dropdown } from 'rsuite';
 
 const ResultDetail = ({
   location,
   open,
   close,
 }) => {
+  const renderHours = () => location.hours.map(hour => {
+    return Object.keys(hour).map((key) => {
+      const dateToFormat = new Date(hour[key][0].date);
+      const openingTime = new Date(hour[key][0].openTimeStamp);
+      const closingTime = new Date(hour[key][0].closeTimeStamp);
+
+      const today = new Date()
+      const yesterday = new Date(today)
+      yesterday.setDate(yesterday.getDate() - 1)
+
+      if (dateToFormat >= yesterday) return (
+        <Dropdown.Item>
+          <b><Moment date={dateToFormat} format={'MMM Do'} />:</b>&nbsp;
+          <Moment date={openingTime} format={'LT'} />&nbsp;-&nbsp;
+          <Moment date={closingTime} format={'LT'} />
+        </Dropdown.Item>
+      );
+    });
+  });
+
   return (
     <Drawer
       show={open}
@@ -27,6 +48,7 @@ const ResultDetail = ({
         </div>
       </Drawer.Header>
       <Drawer.Body>
+        <button></button>
         <h1>{location.name}</h1>
         <div className="result-card-content-wrapper">
           <img className="address-icon" src={pinIcon} alt="Address icon" />
@@ -38,14 +60,10 @@ const ResultDetail = ({
           <span class="icon is-small">
             <FontAwesomeIcon icon={faClock} />
           </span>
-          <Dropdown title="Hours">
-            <Dropdown.Item>New File</Dropdown.Item>
-            <Dropdown.Item>New File with Current Profile</Dropdown.Item>
-            <Dropdown.Item>Download As...</Dropdown.Item>
-            <Dropdown.Item>Export PDF</Dropdown.Item>
-            <Dropdown.Item>Export HTML</Dropdown.Item>
-            <Dropdown.Item>Settings</Dropdown.Item>
-            <Dropdown.Item>About</Dropdown.Item>
+          <Dropdown 
+            title="Hours"
+          >
+            {renderHours()}
           </Dropdown>
         </div>
       </Drawer.Body>
