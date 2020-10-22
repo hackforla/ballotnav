1ae1f6646e844cb8dc2079fcd2accdee6d9e027e
Jake Mensch <jake.mensch@gmail.com>
2020-10-21 21:44:43 -0700

    validate middleware

--- backend/routes2/admin/user.js
+++ backend/routes2/admin/user.js
@@ -1,11 +1,22 @@
-const { checkSchema } = require('express-validator')
-
-const router = require('express').Router()
+const router = require('express-promise-router')()
 const control = require('@controllers/user')
 const auth = require('@middleware/auth')
+const validate = require('@middleware/validate')
 
 router.get('/', auth(['volunteer', 'admin']), control.getUser)
 router.post('/register', control.register)
 router.post('/login', control.login)
+
+router.post(
+  '/test',
+  validate({
+    id: {
+      in: ['query'],
+      errorMessage: 'id required',
+      isInt: true,
+    }
+  }),
+  async (req, res) => res.json({ test: 'how' })
+)
 
 module.exports = router
