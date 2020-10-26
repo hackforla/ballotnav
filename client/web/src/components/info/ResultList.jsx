c6bd10697b8b42cf457d6c90ee2ecaaba95f9ad7
tan-nate <nktan93@gmail.com>
2020-10-25 18:45:51 -0700

    working on desktop compatibility

--- client/web/src/components/info/ResultList.jsx
+++ client/web/src/components/info/ResultList.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ResultHeader from './ResultHeader';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
 
 import { Drawer } from 'rsuite';
 import { ButtonToolbar } from 'rsuite';
@@ -14,7 +15,13 @@ const ResultList = ({
   data,
   toggleCountyInfo,
 }) => {
-  const [open, setOpen] = useState(false);
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1224px)'
+  });
+
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+
+  const [open, setOpen] = useState(true);
 
   const close = () => {
     setOpen(false);
@@ -32,28 +39,36 @@ const ResultList = ({
 
   return (
     <div className="result-list">
-      <ButtonToolbar>
-        <Button onClick={toggleDrawer}>View Drop Off List</Button>
-      </ButtonToolbar>
-      <Drawer
-        show={open}
-        onHide={close}
-        placement="bottom"
-        size="md"
-        className="result-list-drawer"
-      >
-        <Drawer.Header>
-          <p>{data.jurisdictionData.locations.length} drop off locations available in <b>{data.jurisdictionData.name}, {data.stateData.name}</b></p>
-          <div className="county-information-wrapper">
-            <img className="info-icon" src={infoIcon} alt="Information icon"/>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a onClick={toggleCountyInfo}>County election information</a>
-          </div>
-        </Drawer.Header>
-        <Drawer.Body id="drawer-body">
+      {isDesktopOrLaptop && <>
+        <div className="result-list-desktop">
           {renderResultCards()}
-        </Drawer.Body>
-      </Drawer>
+        </div>
+      </>}
+      
+      {isTabletOrMobile && <>
+        <ButtonToolbar>
+          <Button onClick={toggleDrawer}>View Drop Off List</Button>
+        </ButtonToolbar>
+        <Drawer
+          show={open}
+          onHide={close}
+          placement="bottom"
+          size="md"
+          className="result-list-drawer"
+        >
+          <Drawer.Header>
+            <p>{data.jurisdictionData.locations.length} drop off locations available in <b>{data.jurisdictionData.name}, {data.stateData.name}</b></p>
+            <div className="county-information-wrapper">
+              <img className="info-icon" src={infoIcon} alt="Information icon"/>
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <a onClick={toggleCountyInfo}>County election information</a>
+            </div>
+          </Drawer.Header>
+          <Drawer.Body id="drawer-body">
+            {renderResultCards()}
+          </Drawer.Body>
+        </Drawer>
+      </>}
     </div>
   );
 };
