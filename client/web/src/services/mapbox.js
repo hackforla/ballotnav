8328cfd0f439df3fa15ec13dc414a15b6306e991
Jake Mensch <jake.mensch@gmail.com>
2020-12-03 15:42:35 -0800

    add mapbox token to env; update deploy workflow

--- client/web/src/services/mapbox.js
+++ client/web/src/services/mapbox.js
@@ -1,13 +1,6 @@
 import mapboxgl from 'mapbox-gl'
 
-/*
-TODO:
-This token and the styleUrl are from Alex Choi's account. Move the style into a
-ballotnav mapbox account, get a token from that account, and import the
-token from process.env instead of hardcoding it here.
-*/
-mapboxgl.accessToken =
-  'pk.eyJ1Ijoib2tkdW5jYW4iLCJhIjoiY2tnYnk5MGNwMGxydjJ6cXFhOGoxdTBzMCJ9.cFaN1ASx3IKkh1RnofRGpw'
-export const styleUrl = 'mapbox://styles/okduncan/ckglh4q9b07ug19qkg2je0mxl'
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
 
+export const styleUrl = 'mapbox://styles/mapbox/streets-v11'
 export default mapboxgl
