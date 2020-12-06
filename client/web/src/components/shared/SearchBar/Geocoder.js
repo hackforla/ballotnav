e7a317fbae67c1bad60392e05a2f7f749d43ccf3
Jake Mensch <jake.mensch@gmail.com>
2020-12-06 13:18:05 -0800

    linting

--- client/web/src/components/shared/SearchBar/Geocoder.js
+++ client/web/src/components/shared/SearchBar/Geocoder.js
@@ -61,14 +61,11 @@ function Geocoder({ center, address, onResult }) {
       accessToken: mapboxgl.accessToken,
       countries: 'us',
       types: 'address, neighborhood, locality, place, district, postcode',
-      bbox: [-85.61, 30.36, -80.84, 35.00],
+      bbox: [-85.61, 30.36, -80.84, 35.0],
       filter: function (item) {
         return item.context
           .map(function (i) {
-            return (
-              i.id.split('.').shift() === 'region' &&
-              i.text === 'Georgia'
-            )
+            return i.id.split('.').shift() === 'region' && i.text === 'Georgia'
           })
           .reduce(function (acc, cur) {
             return acc || cur
