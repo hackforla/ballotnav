9dbc581cd026590ae39f602347ea6ee4f01b331c
Adam Kendis <atkendis@gmail.com>
2020-12-08 14:48:25 -0800

    fix mixpanel environment check

--- client/web/src/services/mixpanel.js
+++ client/web/src/services/mixpanel.js
@@ -1,10 +1,11 @@
 import * as mxpnl from 'mixpanel-browser'
 
-const PRODUCTION_HOST = 'ballotnav.org'
+const PRODUCTION_HOST = 'www.ballotnav.org'
 const devToken = process.env.REACT_APP_MIXPANEL_TOKEN_DEV
 const prodToken = process.env.REACT_APP_MIXPANEL_TOKEN_PROD
 
-if (window.location.hostname.toLowerCase().search(PRODUCTION_HOST) < 0) mxpnl.init(devToken)
+// initialize with dev token for hostname other than PRODUCTION_HOST
+if (PRODUCTION_HOST.toLowerCase().search(window.location.hostname) < 0) mxpnl.init(devToken)
 else mxpnl.init(prodToken)
 
 const mixpanel = {
