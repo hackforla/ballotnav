3ca530a3b437ff8b28d7463fb7d0a4a40f204546
Adam Kendis <atkendis@gmail.com>
2020-12-08 17:45:19 -0800

    linting

--- client/web/src/services/hotjar.js
+++ client/web/src/services/hotjar.js
@@ -1,19 +1,17 @@
 import { hotjar as hj } from 'react-hotjar'
 
 const PRODUCTION_HOST = 'www.ballotnav.org'
-const devId = process.env.REACT_APP_HOTJAR_ID_DEV
-const prodId = process.env.REACT_APP_HOTJAR_ID_PROD
-const snippetVersion = 6
+const SNIPPET_VERSION = 6
 
 const hotjar = {
   initialize: () => {
-    let id;
+    let id
     if (PRODUCTION_HOST.toLowerCase().search(window.location.hostname) < 0)
-      id = devId
-    else id = prodId
+      id = process.env.REACT_APP_HOTJAR_ID_DEV
+    else id = process.env.REACT_APP_HOTJAR_ID_PROD
 
-    if (id) hj.initialize(id, snippetVersion)
-  }
+    if (id) hj.initialize(id, SNIPPET_VERSION)
+  },
 }
 
 export default hotjar
