1264b1af6d84c11e7a959a324973932fdc7ce924
jafow <jared.a.fowler@gmail.com>
2020-12-12 11:33:05 -0800

    Combine prd & dev infra for cost savings
    support dev & prd under a single lb and cluster;
    
    compress dev and prd configuration into a single config
    for cost savings.
    
    - use a single ALB with host header routing

--- backend/infrastructure/outputs.tf
+++ backend/infrastructure/outputs.tf
@@ -29,3 +29,37 @@ output service_iam_role {
 output service_desired_count {
   value = aws_ecs_service.svc.desired_count
 }
+
+output prd_ecs_service_id {
+  value = aws_ecs_service.svc_prd.id
+}
+
+output prd_ecs_service_name {
+  description = "The name of the prd ECS service"
+  value       = aws_ecs_service.svc_prd.name
+}
+
+output default_target_group_name {
+  description = "target group name for the default deployment (dev)"
+  value       = aws_lb_target_group.default.name
+}
+
+output default_target_group_arn {
+  description = "target group ARN for the default deployment (dev)"
+  value       = aws_lb_target_group.default.arn
+}
+
+output prd_target_group_name {
+  description = "target group name for the prd deployment (prd)"
+  value       = aws_lb_target_group.prd_target_group.name
+}
+
+output prd_target_group_arn {
+  description = "target group ARN for the prd deployment (prd)"
+  value       = aws_lb_target_group.prd_target_group.arn
+}
+
+output prd_acm_cert_arn {
+  description = "the TLS cert used for routing requests to the prod target group"
+  value       = data.aws_acm_certificate.prd_cert.arn
+}
