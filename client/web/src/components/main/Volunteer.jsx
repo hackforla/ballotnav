dc84777ec4f118382ec90f3ac4adc37a2d5aef44
tan-nate <nktan93@gmail.com>
2020-12-12 22:50:13 -0600

    updated copy and styling of volunteer page

--- client/web/src/components/main/Volunteer.jsx
+++ client/web/src/components/main/Volunteer.jsx
@@ -10,43 +10,44 @@ const Volunteer = () => {
         <p>BallotNav is looking for the following types of volunteers:</p>
         <div className="header-wrapper">
           <div className="header-content">
-            <h2>Community Data Monitor</h2>
+            <h2 className="red">Urgent: </h2>
+            <h2>Community Data Monitors</h2>
             <p>
-              Community Data Monitors We need an army of people to collect
-              information about drop-off sites for 3,000 counties, and remain
-              on-call to update it when changes occur. If you are already a
-              volunteer, go to your dashboard.
+              We need an army of volunteers who can validate information about 
+              drop-off sites by calling local election officials. 
+            </p>
+            <h2>Marketing & PR</h2>
+            <p>
+              We need marketing and PR gurus who can help us spread the word 
+              about BallotNav and cultivate partnerships that will allow us to 
+              expand.
             </p>
           </div>
           <img src={volunteerHero} alt="" />
         </div>
         <div className="cards-wrapper">
           <div className="volunteer-card">
-            <h3>Marketing, PR & Partnerships</h3>
-            <p>Lorem ipsum</p>
-          </div>
-          <div className="volunteer-card">
             <h3>Fundraising</h3>
-            <p>Lorem ipsum</p>
+            <p>
+              As we grow, we will need funds to enhance our app infrastructure 
+              and marketing efforts.
+            </p>
           </div>
           <div className="volunteer-card">
             <h3>Software Developers & Data Scientists</h3>
-            <p>Slurm Ipzm</p>
+            <p>
+              We always need devs who can reinvision who we are and propel us into the future.
+            </p>
           </div>
           <div className="volunteer-card">
             <h3>Translators</h3>
-            <p>Spanish especially</p>
+            <p>
+              Our app's greatest flaw is its limitation to English, and we desperately need 
+              translators who can help us better serve America's diverse communities. 
+            </p>
           </div>
         </div>
         <div className="apply-wrapper">
-          <p>
-            If you are already a{' '}
-            <span className="color-red bold">
-              Code for America Brigade member
-            </span>
-            , please visit #voting-rights in the Code for America Slack org to
-            onboard.
-          </p>
           <div>
             <button>Apply</button>
           </div>
