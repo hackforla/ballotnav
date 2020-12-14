fa9c2e0d576dd5f5400788f536833b78dc9b3d4e
Jake Mensch <jake.mensch@gmail.com>
2020-12-13 17:58:13 -0800

    using live data in VerifyBox

--- client/web/src/components/MapPage/shared/LocationDetail/VerifyBox.js
+++ client/web/src/components/MapPage/shared/LocationDetail/VerifyBox.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
 import { ReactComponent as NewWindowIcon } from 'assets/icons/new-window.svg'
 import { ReactComponent as PhoneIcon } from 'assets/icons/phone.svg'
@@ -19,31 +20,62 @@ const useStyles = makeStyles((theme) => ({
   info: {
     display: 'flex',
     alignItems: 'center',
+    flexWrap: 'wrap',
   },
-  link: {
-    display: 'inline-block',
-    margin: '0 18px 0 6px',
-    textDecoration: 'underline',
-  },
-  phone: {
-    display: 'inline-block',
-    marginLeft: 6,
+  infoItem: {
+    display: 'flex',
+    alignItems: 'center',
+    whiteSpace: 'nowrap',
+    margin: '10px 20px 10px 0',
+    '& > a': {
+      display: 'inline-block',
+      marginLeft: 6,
+      textDecoration: 'underline',
+      color: theme.palette.primary.main,
+    },
   },
 }))
 
 const VerifyBox = ({ location, jurisdiction }) => {
   const classes = useStyles()
+
+  if (!jurisdiction) return null
+  const { phones, urls } = jurisdiction
+
+  // TODO: use type names instead of type ids
+  const phone = phones.find(ph => ph.phoneNumberTypeId === 1)?.phoneNumber
+  const url = urls.find(url => url.urlTypeId === 10)?.url
+
+  if (!phone && !url) return null
   return (
     <div className={classes.root}>
       <div className={classes.title}>To Verify</div>
       <div className={classes.info}>
-        <NewWindowIcon />
-        <span className={classes.link}>fayeettetn.us</span>
-        <PhoneIcon />
-        <span className={classes.phone}>(123) 455-3467</span>
+        {phone && (
+          <div className={classes.infoItem}>
+            <PhoneIcon />
+            <a href={`tel:${phone}`}>{phone}</a>
+          </div>
+        )}
+        {url && (
+          <div className={classes.infoItem}>
+            <NewWindowIcon />
+            <a href={url}>{url}</a>
+          </div>
+        )}
       </div>
     </div>
   )
 }
 
 export default VerifyBox
+
+VerifyBox.propTypes = {
+  location: PropTypes.shape({}),
+  jurisdiction: PropTypes.shape({}),
+}
+
+VerifyBox.defaultProps = {
+  location: null,
+  jurisdiction: null,
+}
