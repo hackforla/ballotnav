2bf5e8b31010b3cb7866c483b918c0ee5a22e7da
Jake Mensch <jake.mensch@gmail.com>
2020-12-22 09:05:59 -0800

    cleaned up selectors

--- client/web/src/store/selectors.js
+++ client/web/src/store/selectors.js
@@ -1,5 +1,36 @@
 import { createSelector } from 'reselect'
 import distance from '@turf/distance'
+import queryString from 'query-string'
+
+/////////////////// ROUTER ///////////////////
+
+const search = (state) => state.router.location.search
+const query = createSelector(
+  [search],
+  (search) => {
+    const parsed = queryString.parse(search)
+    return {
+      jid: +parsed.jid || null,
+      lid: +parsed.lid || null,
+      lng: +parsed.lng || null,
+      lat: +parsed.lat || null,
+      address: parsed.address || null,
+    }
+  },
+)
+
+export const selectedJurisdictionId = createSelector([query], (q) => q.jid)
+export const selectedLocationId = createSelector([query], (q) => q.lid)
+export const userAddress = createSelector([query], (q) => q.address)
+
+const userLng = createSelector([query], (q) => q.lng)
+const userLat = createSelector([query], (q) => q.lat)
+export const userLocation = createSelector(
+  [userLng, userLat],
+  (lng, lat) => lng && lat ? { lng, lat } : null
+)
+
+/////////////////// DATA ///////////////////
 
 export const isLoading = (state) => state.data.isLoading
 export const state = (state) => state.data.state
@@ -7,20 +38,6 @@ export const jurisdiction = (state) => state.data.jurisdiction
 export const locations = (state) => state.data.locations
 export const statesWithJurisdictions = (state) =>
   state.data.statesWithJurisdictions
-export const modals = (state) => state.modals
-
-export const selectedJurisdictionId = (state) => +state.router.location.query.jid
-export const selectedLocationId = (state) => +state.router.location.query.lid
-export const userLng = (state) => +state.router.location.query.lng
-export const userLat = (state) => +state.router.location.query.lat
-export const userAddress = (state) => {
-  const { address } = state.router.location.query
-  return address ? decodeURIComponent(address) : undefined
-}
-
-export const userLocation = createSelector([userLng, userLat], (lng, lat) =>
-  lng && lat ? { lng, lat } : null
-)
 
 const geocodedLocations = createSelector([locations], (locations) => {
   if (!locations) return []
@@ -34,6 +51,12 @@ const geocodedLocations = createSelector([locations], (locations) => {
 
   return filteredLocations
 })
+
+/////////////////// MODALS ///////////////////
+
+export const modals = (state) => state.modals
+
+/////////////////// COMBINED ///////////////////
 
 /*
   Adds a 'distanceFromUser' property to each geocoded location and sorts
