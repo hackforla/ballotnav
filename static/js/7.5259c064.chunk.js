f1283cf42c20292324271d682433baaacf3bdda9
Jake Mensch <jake.mensch@gmail.com>
2021-03-10 17:04:00 -0800

    Updates

---
+++ static/js/7.5259c064.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpweb=this.webpackJsonpweb||[]).push([[7],[,function(o,t){window.location="https://about.ballotnav.org"}]]);
+//# sourceMappingURL=7.5259c064.chunk.js.map
--- static/js/main.8aa0aa17.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpweb=this.webpackJsonpweb||[]).push([[0],[function(o,t,n){window.location="https://about.ballotnav.org"}],[[0,1]]]);
-//# sourceMappingURL=main.8aa0aa17.chunk.js.map
