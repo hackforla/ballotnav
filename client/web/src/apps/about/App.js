b57ac4c13a8115b9ce8f926f3c08e4cc897c301d
Jake Mensch <jake.mensch@gmail.com>
2021-03-30 15:35:25 -0700

    remove div100vh from about app

--- client/web/src/apps/about/App.js
+++ client/web/src/apps/about/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Routes from './Routes'
-import Div100vh from 'react-div-100vh'
 import Footer from 'components/core/Footer'
 import CookieConsentBanner from 'components/core/CookieConsentBanner'
 
@@ -9,22 +8,24 @@ const useStyles = makeStyles({
   root: {
     display: 'flex',
     flexDirection: 'column',
+    minHeight: '100vh',
   },
   content: {
     flex: 1,
+    position: 'relative',
   },
 })
 
 const App = () => {
   const classes = useStyles()
   return (
-    <Div100vh className={classes.root}>
+    <div className={classes.root}>
       <div className={classes.content}>
         <Routes />
       </div>
       <Footer />
       <CookieConsentBanner />
-    </Div100vh>
+    </div>
   )
 }
 
