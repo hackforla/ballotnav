4bbb84fe1320835f547591894184d042f30f36f1
Jake Mensch <jake.mensch@gmail.com>
2021-03-30 13:49:20 -0700

    remove sass import from About app

--- client/web/src/apps/about/index.js
+++ client/web/src/apps/about/index.js
@@ -5,7 +5,6 @@ import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from 'theme'
 import App from './App'
-import 'styles/styles.scss'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
