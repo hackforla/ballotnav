b112f6985cb0075e87e1cebbfabd5039b81aae84
Jake Mensch <jake.mensch@gmail.com>
2021-04-08 13:31:35 -0700

    remove admin reducer

--- client/web-admin-2/src/store/index.js
+++ client/web-admin-2/src/store/index.js
@@ -4,14 +4,12 @@ import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 
 import { reducer as auth, types as authTypes } from './actions/auth'
 import { reducer as wip } from './actions/wip'
-import { reducer as admin } from './actions/admin'
 import { reducer as assignment } from './actions/assignment'
 import { reducer as toaster } from './actions/toaster'
 
 const appReducer = combineReducers({
   auth,
   wip,
-  admin,
   assignment,
   toaster,
 })
