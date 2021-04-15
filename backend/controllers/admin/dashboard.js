13260a56d114da2b3b956e223ef6193ed370fb29
Jake Mensch <jake.mensch@gmail.com>
2021-04-15 12:25:13 -0700

    dashboard gis routes

--- backend/controllers/admin/dashboard.js
+++ backend/controllers/admin/dashboard.js
@@ -9,3 +9,54 @@ exports.getJurisdictions = async (req, res) => {
   })
   return res.json(data)
 }
+
+exports.getGisStates = async (req, res) => {
+  const data = await req.db.sequelize.query(
+    `
+      SELECT name, statefp
+        FROM gis_shapes
+        WHERE dataset = 'cb_us_state'
+    `,
+    {
+      type: req.db.Sequelize.QueryTypes.SELECT,
+    }
+  )
+  return res.json(data)
+}
+
+exports.getGisCounties = async (req, res) => {
+  const { statefp } = req.params
+
+  const data = await req.db.sequelize.query(
+    `
+      SELECT name, countyfp, geo
+        FROM gis_shapes
+        WHERE dataset = 'cb_us_county' AND statefp = :statefp
+    `,
+    {
+      replacements: { statefp },
+      type: req.db.Sequelize.QueryTypes.SELECT,
+    }
+  )
+
+  return res.json(data)
+}
+
+exports.getCountyByLonLat = async (req, res) => {
+  let { lon, lat } = req.body
+  
+  const data = await req.db.sequelize.query(
+    `
+    SELECT *
+    	FROM gis_shapes as gs
+    	WHERE dataset IN ('cb_us_county', 'cb_us_state') AND
+        ST_Contains (gs.geo , ST_PointFromText ('POINT(:lon :lat)' , 4326))
+    `,
+    {
+      replacements: { lon: +lon, lat: +lat },
+      type: req.db.Sequelize.QueryTypes.SELECT,
+    }
+  )
+
+  return res.json(data)
+}
