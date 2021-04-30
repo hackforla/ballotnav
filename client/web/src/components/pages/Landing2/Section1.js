0f38c6f881e623c9cfece270fa3998615d249dbb
Jake Mensch <jake.mensch@gmail.com>
2021-04-29 17:39:22 -0700

    Section1 mobile layout

--- client/web/src/components/pages/Landing2/Section1.js
+++ client/web/src/components/pages/Landing2/Section1.js
@@ -2,17 +2,19 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import LaptopHero from 'assets/images/about-hero.svg'
+import useBreakpoints from 'hooks/useBreakpoints'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    // outline: '3px green solid',
     marginTop: '2em',
     display: 'flex',
   },
   left: {
-    width: '50%',
+    width: ({ isDesktop }) => isDesktop ? '50%' : '100%',
     height: '100%',
     paddingRight: '1em',
+    display: 'flex',
+    flexDirection: 'column',
   },
   right: {
     width: '50%',
@@ -36,6 +38,7 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: 700,
     textTransform: 'none',
     fontSize: 20,
+    alignSelf: ({ isDesktop }) => isDesktop ? 'flex-start' : 'center',
   },
   hero: {
     width: '100%',
@@ -44,17 +47,24 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const Section1 = () => {
-  const classes = useStyles()
+  const { isDesktop, isMobile } = useBreakpoints()
+  const classes = useStyles({ isDesktop })
 
   return (
     <div className={classes.root}>
       <div className={classes.left}>
-        <h1 className={classes.heading}>BallotNav is Validating <br /> Information for Future Elections. </h1>
+        <h1 className={classes.heading}>
+          BallotNav is Validating <br />
+          Information for Future Elections.
+        </h1>
         <p className={classes.message}>
           BallotNav helps voters find safe, secure, in-person locations to drop
           off your mail-in or absentee ballot. Launch our demo to navigate
           BallotNav.
         </p>
+        {isMobile && (
+          <img className={classes.hero} src={LaptopHero} />
+        )}
         <Button
           className={classes.button}
           color="primary"
@@ -64,9 +74,11 @@ const Section1 = () => {
           Launch the Demo!
         </Button>
       </div>
-      <div className={classes.right}>
-        <img src={LaptopHero} />
-      </div>
+      {isDesktop && (
+        <div className={classes.right}>
+          <img className={classes.hero} src={LaptopHero} />
+        </div>
+      )}
     </div>
   )
 }
