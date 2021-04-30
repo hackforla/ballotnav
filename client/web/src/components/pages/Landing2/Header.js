7970c7ba3d38adb0376687dcdd96a59b1e92d04b
Jake Mensch <jake.mensch@gmail.com>
2021-04-29 17:05:29 -0700

    Landing2 header

--- client/web/src/components/pages/Landing2/Header.js
+++ client/web/src/components/pages/Landing2/Header.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import ballotNavLogo from 'assets/logos/ballotnav.svg'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    outline: '3px blue solid',
-    height: 100,
+    padding: '1.5em 0 0.5em'
   },
 }))
 
 const Header = () => {
   const classes = useStyles()
 
-  return <div className={classes.root}>Header</div>
+  return (
+    <div className={classes.root}>
+      <img src={ballotNavLogo} />
+    </div>
+  )
 }
 
 export default Header
