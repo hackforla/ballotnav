02fbd7d5b780255bf5f9a6a5643920cc3cdd33dc
Jake Mensch <jake.mensch@gmail.com>
2021-05-05 09:48:40 -0700

    add db info to /status output

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -3,8 +3,12 @@ const router = require('express-promise-router')()
 router.get('/status', (req, res) =>
   res.status(200).json({
     uptime: process.uptime(),
-    build_version: process.env.BUILD_VERSION || 'missing build version',
-    built_at: process.env.BUILT_AT || 'missing built at date',
+    database: process.env.DB_DATABASE,
+    databaseUsername: process.env.DB_USERNAME,
+
+    // TODO: add these env vars to build process?
+    // build_version: process.env.BUILD_VERSION || 'missing build version',
+    // built_at: process.env.BUILT_AT || 'missing built at date',
   })
 )
 
