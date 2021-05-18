e6c89c5cac6ae831a45d394acf94e9a2fe621933
alligatormonday <joey@joeyjepson.com>
2021-05-17 17:44:25 -0700

    removed old landing page files and renamed Landing2 folder to Landing

--- client/web/src/apps/about/Routes.js
+++ client/web/src/apps/about/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import Landing from 'components/pages/Landing2'
+import Landing from 'components/pages/Landing'
 import Privacy from 'components/pages/Privacy'
 
 const Routes = () => {
